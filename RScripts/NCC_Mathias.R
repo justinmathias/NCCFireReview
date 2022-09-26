6840bcb449f29f0d0df1bd7ff1fcfbd8de57d817
justinmathias <justin.m.mathias@gmail.com>
2022-09-26 15:12:18 -0700

    Added RScripts folder and NCC_Mathias

---
+++ RScripts/NCC_Mathias.R
@@ -0,0 +1,3 @@
+#####################Created on 9/26/2022 by Justin Mathias#####################
+##This script will serve as a central hub for commonly utilized functions for ##
+##the NCC Fire Review.
