c7ca58ff6b22b94ab57f7f4bd96223f19fca17af
justinmathias <justin.m.mathias@gmail.com>
2022-11-22 09:30:35 -0500

    Add script for shiny app

---
+++ RScripts/NCC ShinyApp.R
@@ -0,0 +1,8 @@
+library(rsconnect)
+#rsconnect::setAccountInfo(name='justin-mathias', token='91DA81CBBF33816AE601DB62C0E35CA6', secret='GDXPcOcJ2+cQ4P5/deRiLDG6x3jzDCdur98hwUW4')
+
+
+
+
+
+rsconnect::deployApp('path/to/your/app')
