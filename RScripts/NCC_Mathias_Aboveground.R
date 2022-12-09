473cd413c59d1865c982f004ea087bb8261171b9
justinmathias <justin.m.mathias@gmail.com>
2022-12-09 11:48:56 -0500

    Stocks converted to numeric

--- RScripts/NCC_Mathias_Aboveground.R
+++ RScripts/NCC_Mathias_Aboveground.R
@@ -8,72 +8,44 @@ libraries(c("terra", "ggsci", "ggthemes", "RColorBrewer", "measurements", "strin
 theme_set(theme_article(base_size = 13)) #Set ggplot2 theme
 
 #Load in data
-agb <- read.xlsx("/Users/justinmathias/Desktop/aboveground_longform_v3.xlsx")
+agb <- read.xlsx("/Users/justinmathias/Desktop/Working NCC/aboveground_longform_v3.xlsx")
 
-##Create UniqueID and separate Lat/Lon
+# Start cleaning of spreadsheet -------------------------------------------
+##Create UniqueID and separate Lat/Lon----
 agb <- agb %>%
-  mutate(UniqueID = paste(RecordID, RecordSubID, sep = "_")) %>% #First create UniqueID to join later
+  mutate(UniqueID = paste(RecordID, RecordSubID_old, sep = "_")) %>% #First create UniqueID to join later
   sep.coords(LatLon) %>% #First separate Latitude and Longitude
   mutate(Lat = as.numeric(Lat), #Make sure numeric
          Lon = as.numeric(Lon))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Create unique columns for Latitude and Longitude
-amap <- aboveground %>%
-  drop_na(LatLon) %>% #Remove NA values only for LatLon column
-  dplyr::select(LatLon, 12,13, 15:29) %>%
-  group_by(LatLon) %>% #Group by unique LatLon and only include one record per site
-  filter(row_number() == 1) %>%
-  sep.coords(LatLon)
-amap[] <- sapply(amap, as.numeric) #Assign all columns as numeric
-str(amap)
-
-
-###Map of binary information for data included in aboveground----
-amap_long <- amap %>%
-  pivot_longer(names_to = "Measurement", values_to = "Binary", -c(Lat,Lon)) %>%
-  separate(Measurement, c("Measurement"), sep = "_Binary_Measurements") #Drop the "_Binary_Measurements" text
-
-msmnts <- unique(amap_long$Measurement)
-for (i in 1:length(msmnts)) {
-  {x <- paste0(msmnts[[i]])
-  obs <- sum(amap_long %>% filter(Measurement == x & Binary == 1) %>% dplyr::select(Binary))
-  fname <- paste0(x, ".tiff")
-  ggplotly(
-    amap_long %>% filter(Measurement == x & Binary == 1) %>%
-      ggplot() + #Plot
-      borders("world", colour = "gray40", fill = "gray99") +
-      theme_article() +
-      coord_fixed(1.2) +
-      geom_point(
-        aes(x = Lon, y = Lat),
-        size = 1.5,
-        color = "#00A087FF"
-      ) +
-      scale_size_continuous(range = c(1, 8),
-                            breaks = c(5, 10, 15)
-      ) +
-      ggtitle(paste0("Aboveground \n", x, "\n", "Obs:", sum(amap_long %>% filter(Measurement == x & Binary == 1) %>% dplyr::select(Binary))))
+##Get all data into numeric format
+AGB <- agb %>%
+  mutate(
+    LiveWoodC_StockData = sep.data(., in_col = LiveWoodC_StockData, return = "Value"),
+    FoliageC_StockData = sep.data(., in_col = FoliageC_StockData, return = "Value"),
+    TotalLive_StockData = sep.data(., in_col = TotalLive_StockData, return = "Value"),
+    PyC_StockData = sep.data(., in_col = PyC_StockData, return = "Value"),
+    CWDC_StockData = sep.data(., in_col = CWDC_StockData, return = "Value"),
+    FWDC_StockData = sep.data(., in_col = FWDC_StockData, return = "Value"),
+    LitterC_StockData = sep.data(., in_col = LitterC_StockData, return = "Value"),
+    TotalDead_StockData = sep.data(., in_col = TotalDead_StockData, return = "Value"),
+    TotalAboveground_StockData = sep.data(., in_col = TotalAboveground_StockData, return = "Value"),
+    TEC_StockData = sep.data(., in_col = TEC_StockData, return = "Value"),
   )
-  ggsave(paste0(fname), units = c("in"), width = 5, height = 4, dpi = 300)
-  }
-}
+
+#Units in area basis
+unique(agb$LiveC_units_StockData)
+
+#Define which units to include
+LiveCarea <- c("Mg_per_hectare", "MgC_per_hectare", "gC_per_m2", "g_per_m2", "kg_per_hectare", "kgC_per_m2",
+                 "kgC_per_hectare", "kg_per_m2", "molC_per_m2" )
+
+agb %>% filter(LiveC_units_StockData %in% LiveCarea)
+
+
+##Clean up units
+agb %>%
+  mutate()
+
 
 
