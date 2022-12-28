9b3f195d8cc8c14c0fa85248af251d6c1b488926
justinmathias <justin.m.mathias@gmail.com>
2022-12-28 13:16:11 -0500

    initial commit of GFED script

---
+++ RScripts/NCC_Mathias_GFED.R
@@ -0,0 +1,9 @@
+#Created by Justin Mathias, 12/28/22
+
+#Housekeeping: load packages, set themes, etc.
+library("easypackages")
+libraries(c("terra", "ggsci", "ggthemes", "RColorBrewer", "measurements", "stringr", "rayshader", "egg", "rgdal", "openxlsx", "shiny", "shinydashboard",
+            "plotly", "wordcloud", "tm", "soilDB", "aqp", "rhdf5", "tidyverse"))
+theme_set(theme_clean(base_size = 13)) #Set ggplot2 theme
+
+/Users/justinmathias/Desktop/GFED4/GFED4.1s_2018_beta.hdf5
